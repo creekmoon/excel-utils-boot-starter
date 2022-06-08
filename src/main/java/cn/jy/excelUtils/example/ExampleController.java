56af6ec3480412d62e5a6fbfccede492c7d491d5
yinjiangyue <841710216@qq.com>
2022-06-08 10:53:20 +0800

    +优化

--- src/main/java/cn/jy/excelUtils/example/ExampleController.java
+++ src/main/java/cn/jy/excelUtils/example/ExampleController.java
@@ -1,12 +1,14 @@
 package cn.jy.excelUtils.example;
 
 import cn.jy.excelUtils.core.ExcelExport;
+import cn.jy.excelUtils.core.ExcelImport;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,7 +20,12 @@ import java.util.Date;
 @RestController("/test")
 public class ExampleController  {
 
-
+    /**
+     * 导出
+     * @param request
+     * @param response
+     * @throws IOException
+     */
     @GetMapping(value = "/exportExcel")
     public void exportExcel( HttpServletRequest request, HttpServletResponse response) throws IOException {
         Student student = new Student();
@@ -40,9 +47,29 @@ public class ExampleController  {
                 .addTitle("学生::邮箱",Student::getEmail)
                 .addTitle("过期时间",Student::getExpTime)
                 .write(result)
+                .write(result)
+                .write(result)
                 .response(response);
 
     }
 
+
+    /**
+     * 导入
+     * @param request
+     * @param response
+     * @throws IOException
+     */
+    @PostMapping(value = "/importExcel")
+    public void importExcel(MultipartFile file, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        ExcelImport.create(file, Student::new)
+                .addConvert("姓名", Student::setUserName)
+                .addConvert("年龄", Integer::valueOf, Student::setAge)
+                .read(student -> {
+                    System.out.println(student);
+                })
+                .response(response);
+
+    }
 
 }
